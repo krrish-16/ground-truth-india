0c1ec5213bd3c60c09bc8a47aae5864924e1ac90
Harsh Patil <harshkishorpatil@gmail.com>
2025-09-09 22:00:17 +0530

    Added Capacitor setup with Android platform

---
+++ android/app/src/main/java/com/groundtruth/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.groundtruth.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.groundtruth.app',
+  appName: 'Ground Truth India',
+  webDir: 'dist'
+};
+
+export default config;
